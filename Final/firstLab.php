3b96c0c1dd46904859757cddd66d9d176fedf2c4
Bikas-cloud <shilb677@gmail.com>
2025-12-14 14:32:41 +0600

    firstlab

---
+++ Final/firstLab.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>First lab PHP Lab</title>
+</head>
+<body>
+    <h1>This is PHP Class</h1>
+    <input type="text" id="txt1" >
+    <input type="text" id="txt2" >
+
+    <?php
+        echo "Hello, World!";
+        $name = "raj roy";
+        $age = 22;
+        echo "I am : ".$name." and my age is : ".$age."<br>";
+$x=10;
+$y=10;
+$z=$x+$y;
+echo $z ;   ?>
+</body>
+</html>
